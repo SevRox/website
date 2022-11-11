bee5df98946814a8d623aeaff7938c965abcf8d7
martinrybka2000 <rybatinek14@gmail.com>
2022-11-11 14:40:44 +0100

    BugFix #1 wront routing to livedata

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { SettingsComponent } from './settings/settings.component';
 import { GraphsComponent } from './graphs/graphs.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/lifedata', pathMatch: 'full' },
+  { path: '', redirectTo: '/livedata', pathMatch: 'full' },
   { path: 'livedata', component: LiveDataComponent },
   { path: 'settings', component: SettingsComponent },
   { path: 'graphs', component: GraphsComponent }
