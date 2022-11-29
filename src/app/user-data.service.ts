e29f2843c7d3dbf199c733f6f10df1c3514026fa
martinrybka2000 <rybatinek14@gmail.com>
2022-11-29 20:48:22 +0100

    Added last chosen board detection in request

--- src/app/user-data.service.ts
+++ src/app/user-data.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { LocalService } from './local.service';
 import { BoardData } from './structs/boards';
 
 @Injectable({
@@ -9,10 +10,19 @@ import { BoardData } from './structs/boards';
 })
 export class UserDataService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private localSore: LocalService,private http: HttpClient) { }
 
   getUserBoards(): Observable<Array<BoardData>> {
-    return this.http.get<Array<BoardData>>(environment.backendUrl + 'boards/web/all/9');
+    return this.http.get<Array<BoardData>>(environment.backendUrl + 'boards/web/all/9').pipe(
+      map((receivedData: Array<BoardData>) => {
+        let lastMac: String = this.localSore.getData("choosenMac");
+        receivedData.forEach(element => {
+          if (element.mac_address == lastMac)
+            element.last_choosen = true;
+        })
+        return receivedData;
+      })
+    );
   }
 
   deleteBoard(board_mac: string) {
